48e2a4ee290f1cc07af59ee21136d9e3117c5730
srojas <fesaroma@outlook.com>
2025-09-10 23:18:05 -0600

    chore: improve outputs  terraform code

--- outputs.tf
+++ outputs.tf
@@ -5,13 +5,13 @@ output "vpce_sg_id" {
   value       = aws_security_group.vpc_endpoints_sg.id
 }
 
-output "s3_vpce_id" {
-  description = "VPC Endpoint ID para S3"
+output "eks_vpce_id" {
+  description = "VPC Endpoint ID para eks"
   value       = module.vpc_endpoints.endpoints.s3.id
 }
 
-output "ssm_vpce_id" {
-  description = "VPC Endpoint ID para SSM"
+output "eks_auth_vpce_id" {
+  description = "VPC Endpoint ID para eks-auth"
   value       = module.vpc_endpoints.endpoints.ssm.id
 }
 
@@ -20,12 +20,7 @@ output "vpce_ids" {
   value = {
     eks         = module.vpc_endpoints.endpoints.eks.id
     eks_auth    = module.vpc_endpoints.endpoints.eks_auth.id
-    ec2         = module.vpc_endpoints.endpoints.ec2.id
-    ec2messages = module.vpc_endpoints.endpoints.ec2messages.id
-    s3          = module.vpc_endpoints.endpoints.s3.id
-    ssm         = module.vpc_endpoints.endpoints.ssm.id
-    ssmmessages = module.vpc_endpoints.endpoints.ssmmessages.id
-    kms         = module.vpc_endpoints.endpoints.kms.id
+    sts        = module.vpc_endpoints.endpoints.sts.id
   }
 }
 
